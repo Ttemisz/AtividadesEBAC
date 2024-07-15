abe83ccd9231481f15a3f7ab9eae0ab8e3d6c9ff
Temistoclis <temistoclisxp58@gmail.com>
2024-07-14 22:34:10 -0300

    ts

---
+++ tsexe/exe.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicacao(num1, num2) {
+    return num1 * num2;
+}
+function saudacao(nome) {
+    return 'Ola' + nome;
+}
+console.log(saudacao('junior'));
+console.log(multiplicacao(1, 2));
